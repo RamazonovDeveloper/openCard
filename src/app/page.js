a50715321673e1fa46a2867cb473b79bb8980bd4
RamazonovDeveloper <ramazonovferuz01@gmail.com>
2023-05-03 18:01:37 +0500

    i18 installed

--- src/app/page.js
+++ src/app/page.js
@@ -1,11 +1,38 @@
 import Image from 'next/image'
+// import React from "react";
+// import i18n from "i18next";
+// import { useTranslation, initReactI18next } from "react-i18next";
+
+
+// i18n
+//   .use(initReactI18next) // passes i18n down to react-i18next
+//   .init({
+//     // the translations
+//     // (tip move them in a JSON file and import them,
+//     // or even better, manage them via a UI: https://react.i18next.com/guides/multiple-translation-files#manage-your-translations-with-a-management-gui)
+//     resources: {
+//       en: {
+//         translation: {
+//           "Welcome to React": "Welcome to React and react-i18next"
+//         }
+//       }
+//     },
+//     lng: "en", // if you're using a language detector, do not define the lng option
+//     fallbackLng: "en",
+
+//     interpolation: {
+//       escapeValue: false // react already safes from xss => https://www.i18next.com/translation-function/interpolation#unescape
+//     }
+//   });
 
 export default function Home() {
   return (
     <div>
 
       <header>
-        
+        <h1>
+          Hello World This is a open Card
+        </h1>
       </header>
       {/* Hello World This is a open Card */}
     </div>
